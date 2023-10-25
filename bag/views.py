78a3f9cf6845e0af5eed1e8892d9f6de734d6489
Kim Bergstrom <kimmen_b@hotmail.com>
2023-10-25 16:25:19 +0000

    feat: finished product size logic

--- bag/views.py
+++ bag/views.py
@@ -12,12 +12,25 @@ def add_to_bag(request, item_id):
     """
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
     bag = request.session.get('bag', {}) # create or update the bag
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity # if the item already in bag, increase the quantity
+    
+    if size: # Check is the item already exist with that size, or not and then increase it
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity # else add the item to the bag
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity # if the item already in bag, increase the quantity
+        else:
+            bag[item_id] = quantity # else add the item to the bag
     
     request.session['bag'] = bag
     return redirect(redirect_url)
