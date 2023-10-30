4a9f1a98823e4ec0a31f1fa8b654413f1723fc51
Kim Bergstrom <kimmen_b@hotmail.com>
2023-10-30 21:22:39 +0000

    add: order summary to checkout success page

--- checkout/views.py
+++ checkout/views.py
@@ -100,10 +100,10 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Order successfully processed! \
-        Your order number is \
-        {order_number}. \
-        A confirmation email \
+    messages.success(request, f'Order successfully processed!/n \
+        Your order number is/n\
+        {order_number}./n \
+        A confirmation email/n \
         will be sent to {order.email}.')
 
     if 'bag' in request.session:
