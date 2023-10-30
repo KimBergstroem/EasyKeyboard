e6f1843823dd033fc3ce17b9af0dedd18970c481
Kim Bergstrom <kimmen_b@hotmail.com>
2023-10-30 13:47:38 +0000

    feat: add env file and stripe config

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from bag.contexts import bag_contents
+import stripe
 
 def checkout(request):
     bag = request.session.get('bag', {})
@@ -8,10 +11,16 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('product'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': STRIPE_PUBLIC_KEY,
+        'client_secret': STRIPE_SECRET_KEY,
     }
 
     return render(request, template, context)
