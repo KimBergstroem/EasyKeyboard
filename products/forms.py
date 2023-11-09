d723b365e42f595dfddf9a2a91b68ebde0a29ec5
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-09 09:07:27 +0000

    feat: admin productform created

---
+++ products/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Product, Category
+
+
+class Productform(form.Modelform):
+    """
+    Extends the form-modelforms built in from django
+    """
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.object.all()
+        friendly_name = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.item():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+
